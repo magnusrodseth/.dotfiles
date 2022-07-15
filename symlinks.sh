76a8fc1199bbf2d8139e9cefeebc34f7ecafee01
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-07-15 13:09:07 +0200

    remove link in symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -4,7 +4,6 @@
 DOTFILES_DIRECTORY="~/.dotfiles"
 
 # Add symlinks between the .dotfiles directory and the ~ directory 
-ln -s "${DOTFILES_DIRECTORY}/.vim" ~/.vim
 ln -s "${DOTFILES_DIRECTORY}/.bashrc" ~/.bashrc
 ln -s "${DOTFILES_DIRECTORY}/.zshrc" ~/.zshrc.pre-oh-my-zsh
 ln -s "${DOTFILES_DIRECTORY}/.zshrc" ~/.zshrc
