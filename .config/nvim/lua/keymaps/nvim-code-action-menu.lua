fe6698c84b93728ff4d4c03ca0c1144991ad2b71
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-07-30 17:38:34 +0200

    update suggested changes shortcut

--- .config/nvim/lua/keymaps/nvim-code-action-menu.lua
+++ .config/nvim/lua/keymaps/nvim-code-action-menu.lua
@@ -2,4 +2,4 @@ local map = require("keymaps.utils").map
 local opts = { noremap = true, silent = true }
 
 -- Suggested actions
-map("n", "<leader>sug", "<cmd>CodeActionMenu<cr>", opts)
+map("n", "<C-s>", "<cmd>CodeActionMenu<cr>", opts)
