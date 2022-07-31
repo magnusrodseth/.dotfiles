3b657aa584eb902e43d509f7bde90074fd409290
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-07-31 11:39:41 +0200

    update symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env zsh 
 
-# Find the .dotfiles directory on the machine
-DOTFILES_DIRECTORY="~/.dotfiles"
+# Adds symlinks between directories and files
+symlink() {
+    from=$1
+    to=$2
 
-# Add symlinks between the .dotfiles directory and the ~ directory 
-ln -s "${DOTFILES_DIRECTORY}/.bashrc" ~/.bashrc
-ln -s "${DOTFILES_DIRECTORY}/.zshrc" ~/.zshrc.pre-oh-my-zsh
-ln -s "${DOTFILES_DIRECTORY}/.zshrc" ~/.zshrc
-ln -s "${DOTFILES_DIRECTORY}/.profile" ~/.profile
-ln -s "${DOTFILES_DIRECTORY}/.bash_profile" ~/.bash_profile
-ln -s "${DOTFILES_DIRECTORY}/.oh-my-zsh" ~/.oh-my-zsh
-ln -s "${DOTFILES_DIRECTORY}/.gitconfig" ~/.gitconfig
-ln -s "${DOTFILES_DIRECTORY}/.config/nvim" ~/.config/nvim
-ln -s "${DOTFILES_DIRECTORY}/.config/lazygit/config.yml" ~/.config/lazygit/config.yml
-ln -s "${DOTFILES_DIRECTORY}/.config/lazydocker/config.yml" ~/.config/lazydocker/config.yml
-ln -s "${DOTFILES_DIRECTORY}/.config/lazydocker/config.yml" /Users/magnusrodseth/Library/Application\ Support/lazydocker/config.yml
-ln -s "${DOTFILES_DIRECTORY}/.ideavimrc" ~/.ideavimrc
-ln -s "${DOTFILES_DIRECTORY}/.vscode" ~/.vscode
-ln -s "~/.dotfiles/vscode/keybindings.json" "~/Library/Application\ Support/Code/User/keybindings.json" 
-ln -s "~/.dotfiles/vscode/keybindings.json" "~/Library/Application\ Support/Code/User/keybindings.json" 
-ln -s "~/.dotfiles/vscode/snippets" "~/Library/Application\ Support/Code/User/snippets" 
-ln -s "~/.dotfiles/ideavim/.ideavimrc" ~/.ideavimrc
+    # Find the .dotfiles directory on the machine
+    DOTFILES_DIRECTORY="~/.dotfiles"
+
+    ln -sf "$DOTFILES_DIRECTORY/$source" "$to"
+}
+
+symlink ".bashrc" "~/.bashrc"
+symlink ".zshrc" "~/.zshrc.pre-oh-my-zsh"
+symlink ".zshrc" "~/.zshrc"
+symlink ".profile" "~/.profile"
+symlink ".bash_profile" "~/.bash_profile"
+symlink ".oh-my-zsh" "~/.oh-my-zsh"
+symlink ".gitconfig" "~/.gitconfig"
+symlink ".config/nvim" "~/.config/nvim"
+symlink ".config/lazygit/config.yml" "~/Library/Application\ Support/lazygit/config.yml"
+symlink ".config/lazydocker/config.yml" "~/Library/Application\ Support/lazydocker/config.yml"
+symlink ".vscode" "~/.vscode"
+symlink ".dotfiles/vscode/keybindings.json" "~/Library/Application\ Support/Code/User/keybindings.json"
+symlink "~/.dotfiles/vscode/snippets" "~/Library/Application\ Support/Code/User/snippets" 
+symlink "~/.dotfiles/ideavim/.ideavimrc" "~/.ideavimrc"
