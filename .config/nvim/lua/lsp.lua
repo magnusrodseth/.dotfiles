05540ad6cdf1e414f797f28e14040f244d59b4ee
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-08-01 19:54:35 +0200

    format

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -13,51 +13,51 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, options)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, buffer_number)
-    lsp_format.on_attach(client)
+  lsp_format.on_attach(client)
 
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(buffer_number, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(buffer_number, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local buffer_options = { noremap = true, silent = true, buffer = buffer_number }
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local buffer_options = { noremap = true, silent = true, buffer = buffer_number }
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, buffer_options)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, buffer_options)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, buffer_options)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, buffer_options)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, buffer_options)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, buffer_options)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, buffer_options)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, buffer_options)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, buffer_options)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, buffer_options)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, buffer_options)
-    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, buffer_options)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, buffer_options)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, buffer_options)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, buffer_options)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, buffer_options)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, buffer_options)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, buffer_options)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, buffer_options)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, buffer_options)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, buffer_options)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, buffer_options)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, buffer_options)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, buffer_options)
 
-    -- Display diagnostics using float, not to the right of the code
-    vim.api.nvim_create_autocmd("CursorHold", {
-        buffer = buffer_number,
-        callback = function()
-            local diagnostic_options = {
-                focusable = false,
-                close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-                border = 'rounded',
-                source = 'always',
-                prefix = ' ',
-                scope = 'cursor',
-            }
-            vim.diagnostic.open_float(nil, diagnostic_options)
-        end
-    })
+  -- Display diagnostics using float, not to the right of the code
+  vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = buffer_number,
+    callback = function()
+      local diagnostic_options = {
+        focusable = false,
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = 'rounded',
+        source = 'always',
+        prefix = ' ',
+        scope = 'cursor',
+      }
+      vim.diagnostic.open_float(nil, diagnostic_options)
+    end
+  })
 end
 
 
 local lsp_flags = {
-    -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
 }
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -65,49 +65,66 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 -- Setup
 local lsp_config = require('lspconfig')
 local servers = {
-    'clangd',
-    'cmake',
-    'cssls',
-    'dockerls',
-    'eslint',
-    'gopls',
-    'graphql',
-    'html',
-    'jdtls',
-    'jsonls',
-    'kotlin_language_server',
-    'lemminx',
-    'ltex',
-    'marksman',
-    'prismals',
-    'pyright',
-    'rust_analyzer',
-    'sumneko_lua',
-    'svelte',
-    'tailwindcss',
-    'tsserver',
-    'yamlls'
+  'clangd',
+  'cmake',
+  'cssls',
+  'dockerls',
+  'eslint',
+  'gopls',
+  'graphql',
+  'html',
+  'jdtls',
+  'jsonls',
+  'kotlin_language_server',
+  'lemminx',
+  'ltex',
+  'marksman',
+  'prismals',
+  'pyright',
+  'rust_analyzer',
+  'sumneko_lua',
+  'svelte',
+  'tailwindcss',
+  'tsserver',
+  'yamlls'
 }
 
 for _, lsp in ipairs(servers) do
-    lsp_config[lsp].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = lsp_flags,
-        settings = {
-            ["rust-analyzer"] = {
-                assist = {
-                    importEnforceGranularity = true,
-                    importPrefix = "crate"
-                },
-                cargo = {
-                    allFeatures = true
-                },
-                checkOnSave = {
-                    -- default: `cargo check`
-                    command = "clippy"
-                },
-            },
-        }
-    })
+  lsp_config[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+    settings = {
+      ["rust-analyzer"] = {
+        assist = {
+          importEnforceGranularity = true,
+          importPrefix = "crate"
+        },
+        cargo = {
+          allFeatures = true
+        },
+        checkOnSave = {
+          -- default: `cargo check`
+          command = "clippy"
+        },
+        inlayHints = {
+          lifetimeElisionHints = {
+            enable = true,
+            useParameterNames = true
+          },
+        },
+      },
+      ["gopls"] = {
+        analyses = {
+          nilness = true,
+          unusedparams = true,
+          unusedwrite = true,
+          useany = true,
+        },
+        gofumpt = true,
+        staticcheck = true,
+        usePlaceholders = true,
+      }
+    }
+  })
 end
