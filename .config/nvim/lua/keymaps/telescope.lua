c8bc8b7717e12f61cf52d724b965e4cd9c43a009
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-09-04 12:58:34 +0200

    search all files, not just git files

--- .config/nvim/lua/keymaps/telescope.lua
+++ .config/nvim/lua/keymaps/telescope.lua
@@ -1,7 +1,7 @@
 local map = require("keymaps.utils").map
 
 -- Respect .gitignore when finding files
-map("n", "<leader>ff", "<cmd>lua require('telescope.builtin').git_files()<cr>")
+map("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
 
 map("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 map("n", "<leader>of", "<cmd>lua require('telescope.builtin').oldfiles()<cr>")
