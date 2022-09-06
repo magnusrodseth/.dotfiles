d5ec47075b18df72ad25c1b77d831c78cacefb4c
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-09-06 10:20:52 +0200

    sheeesh

--- .config/nvim/lua/keymaps/harpoon.lua
+++ .config/nvim/lua/keymaps/harpoon.lua
@@ -2,7 +2,7 @@ local map = require("keymaps.utils").map
 
 map("n", "ham", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>")
 map("n", "haa", "<cmd>lua require('harpoon.mark').add_file()<cr>")
-map("n", "<C-1>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>")
-map("n", "<C-2>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>")
-map("n", "<C-3>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>")
-map("n", "<C-4>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>")
+map("n", "ht1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>")
+map("n", "ht2>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>")
+map("n", "ht3>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>")
+map("n", "ht4>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>")
